6e71036c2ad66c4a7b264347dd09c6b8923e7403
Michael Daou <daoumichael@gmail.com>
2015-11-11 19:46:03 -0500

    Created abstract object editor.

---
+++ working/editor/sidepanes/AObjectEditor.java
@@ -0,0 +1,15 @@
+package editor.sidepanes;
+
+import javafx.scene.layout.GridPane;
+
+public abstract class AObjectEditor {
+	
+	private GridPane myEditorPane;
+	
+	public GridPane getEditorPane(){
+		return myEditorPane;
+	}
+	
+	
+
+}
