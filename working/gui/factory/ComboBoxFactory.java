fb89071caeb697d4a4be580f16540f224dda8f93
Michael Daou <daoumichael@gmail.com>
2015-11-12 23:29:27 -0500

    fixed a merge error.

--- working/gui/factory/ComboBoxFactory.java
+++ working/gui/factory/ComboBoxFactory.java
@@ -1,37 +1,5 @@
 package gui.factory;
 
-import java.util.ResourceBundle;
-
-import javafx.scene.control.ComboBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import resources.ResourceRetriever;
-
-public class ComboBoxFactory {
-	public ComboBoxFactory(){
-	}
-	
-	private ComboBox imageComboBox(){
-		return null;
-	}
-
-	private ComboBox<String> createImageComboBox() {
-		ComboBox<String> comboBox = new ComboBox<String>();
-        comboBox.setPromptText(ResourceRetriever.getLabel("TileSelectPrompt"));
-        comboBox.getItems().add(ResourceRetriever.getLabel("SceneryTile"));
-        comboBox.getItems().add(ResourceRetriever.getLabel("PathTile"));
-        //comboBox.valueProperty().addListener((o, s1, s2) -> showImageOptions(s2));
-		return comboBox;
-	}
-
-
-
-}
-
-
-
-package gui.factory;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ResourceBundle;
