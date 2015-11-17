12a102182ecbb5f8e66f906b9e961d3bce482647
Michael Daou <daoumichael@gmail.com>
2015-11-16 21:44:01 -0500

    Worked on ImageTile, will now create a separate branch.

--- working/editor/ImageTile.java
+++ working/editor/ImageTile.java
@@ -2,10 +2,34 @@ package editor;
 
 import java.util.ResourceBundle;
 
-public class ImageTile {
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
-	public ImageTile(ResourceBundle tileIconBundle) {
-		// TODO Auto-generated constructor stub
+public class ImageTile extends ImageView {
+
+	public ImageTile(String s, ResourceBundle tileIconBundle, int[] dimensions) {
+		String[] tileIconPath = tileIconBundle.getString(s).split(",");
+		//
+		for (int i = 0; i < tileIconPath.length; i++) {
+			ImageView img = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream(tileIconPath[i])));
+			
+			
+			img.focusedProperty().addListener((o,oldValue,newValue) -> {
+		        if (newValue) {
+		            img.setEffect(new Glow(0.7));
+		        }
+		        else {
+		            img.setEffect(null);
+		        }});
+			
+			img.setFitWidth(dimensions[0]);
+			img.setFitHeight(dimensions[1]);
+			//iconPane.add(img, i % NUMBER_COLUMN_ICON_PANEL, i / NUMBER_COLUMN_ICON_PANEL, 1, 1);
+		}
+		
+		//iconPane.add(createOkButton(s),0, tileIconPath.length / NUMBER_COLUMN_ICON_PANEL + 1, 1, 1);
 	}
 
 }
