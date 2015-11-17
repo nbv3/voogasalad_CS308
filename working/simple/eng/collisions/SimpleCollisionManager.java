a7d837dc94c0a1067789841453bc0983c3188cdd
Nick von Turkovich <nbv3@duke.edu>
2015-11-17 17:47:51 -0500

    Manager created

--- working/simple/eng/collisions/SimpleCollisionManager.java
+++ working/simple/eng/collisions/SimpleCollisionManager.java
@@ -27,25 +27,25 @@ public class SimpleCollisionManager{
 		}
 	}
 	
-	private static Boolean intersects(ISimpleObject obj, ISimpleObject other) {
-		BoundingBox abox = obj.getBoundingBox();
-		BoundingBox bbox = other.getBoundingBox();
-
-		double ax = abox.getPoint().getX();
-		double bx = bbox.getPoint().getX();
-		double ay = abox.getPoint().getY();
-		double by = bbox.getPoint().getY();
-
-		double aw = abox.getWidth();
-		double bw = bbox.getWidth();
-		double ah = abox.getHeight();
-		double bh = bbox.getHeight();
-
-		Boolean aInsideBX = (ax + aw >= bx && ax <= bx + bw);
-		Boolean aInsideBY = (ay + ah >= by && ay <= by + bh);
-		Boolean bInsideAX = (bx + bw >= ax && bx <= ax + aw);
-		Boolean bInsideAY = (by + bh >= ay && by <= ay + ah);
-		return ((aInsideBX && aInsideBY) || (bInsideAX && bInsideAY));
-	}
+//	private static Boolean intersects(ISimpleObject obj, ISimpleObject other) {
+//		BoundingBox abox = obj.getBoundingBox();
+//		BoundingBox bbox = other.getBoundingBox();
+//
+//		double ax = abox.getPoint().getX();
+//		double bx = bbox.getPoint().getX();
+//		double ay = abox.getPoint().getY();
+//		double by = bbox.getPoint().getY();
+//
+//		double aw = abox.getWidth();
+//		double bw = bbox.getWidth();
+//		double ah = abox.getHeight();
+//		double bh = bbox.getHeight();
+//
+//		Boolean aInsideBX = (ax + aw >= bx && ax <= bx + bw);
+//		Boolean aInsideBY = (ay + ah >= by && ay <= by + bh);
+//		Boolean bInsideAX = (bx + bw >= ax && bx <= ax + aw);
+//		Boolean bInsideAY = (by + bh >= ay && by <= ay + ah);
+//		return ((aInsideBX && aInsideBY) || (bInsideAX && bInsideAY));
+//	}
 
 }
---
+++ working/simple/manager/SimpleGameManager.java
@@ -0,0 +1,13 @@
+package simple.manager;
+
+import simple.universe.ISimpleUniverse;
+
+public class SimpleGameManager {
+	
+	private ISimpleUniverse myUniverse;
+	// private ISimpleViewController myViewController;
+	//
+	
+	
+
+}
