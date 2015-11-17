f08078ffe4626ac50a95b4e5a6ee4029dd4211a3
Nick von Turkovich <nbv3@duke.edu>
2015-11-16 21:30:48 -0500

    CollisionManager editor

--- working/engine/CollisionManager.java
+++ working/engine/CollisionManager.java
@@ -2,57 +2,44 @@ package engine;
 
 import java.util.List;
 
-import engine.environment.IEnvironment;
 import objects.IGameObject;
 import view.BoundingBox;
 
 public class CollisionManager implements ICollisionManager {
-	
-	private IGameEngine myEngine;
-	
-	public CollisionManager (IGameEngine engine) {
-		myEngine = engine;
-	}
 
 	@Override
-	public void update() {
-		List<IGameObject> objs = getEnvironment().getEnvironmentObjects();
-		for (IGameObject obj: objs) {
-			checkCollision(obj, objs);
-		}
-		
-	}
-	
-	private void checkCollision(IGameObject obj, List<IGameObject> objs) {
-		for (IGameObject other: objs) {
-			if (other.equals(obj)) {
-				continue;
-			}
-			
-			if (intersects(obj, other)) {
-				obj.onCollision(other);
-				other.onCollision(obj);
-				System.out.println("INTERSECTING");
+	public void checkCollisions(List<IGameObject> list) {
+		for (IGameObject object : list) {
+
+			for (IGameObject other : list) {
+				if (other.equals(object)) {
+					continue;
+				}
+				if (intersects(object, other)) {
+
+					System.out.println("INTERSECTING");
+				}
+
 			}
 		}
 	}
-	
+
 	private Boolean intersects(IGameObject obj, IGameObject other) {
 		BoundingBox abox = obj.getBoundingBox();
 		BoundingBox bbox = other.getBoundingBox();
-		
+
 		double ax = abox.getPoint().getX();
 		double bx = bbox.getPoint().getX();
 		double ay = abox.getPoint().getY();
 		double by = bbox.getPoint().getY();
-		
+
 		double aw = abox.getWidth();
 		double bw = bbox.getWidth();
 		double ah = abox.getHeight();
 		double bh = bbox.getHeight();
-		
+
 		Boolean doesIntersect = false;
-		
+
 		Boolean aInsideBX = (ax + aw >= bx && ax <= bx + bw);
 		Boolean aInsideBY = (ay + ah >= by && ay <= by + bh);
 		Boolean bInsideAX = (bx + bw >= ax && bx <= ax + aw);
@@ -60,12 +47,8 @@ public class CollisionManager implements ICollisionManager {
 		if ((aInsideBX && aInsideBY) || (bInsideAX && bInsideAY)) {
 			doesIntersect = true;
 		}
-		
+
 		return doesIntersect;
-	}
-	
-	private IEnvironment getEnvironment() {
-		return myEngine.getEnvironment();
 	}
 
 }
--- working/engine/ICollisionManager.java
+++ working/engine/ICollisionManager.java
@@ -1,7 +1,11 @@
 package engine;
 
+import java.util.List;
+
+import objects.IGameObject;
+
 public interface ICollisionManager {
 	
-	public void update();
+	public void checkCollisions(List<IGameObject> list);
 
 }
