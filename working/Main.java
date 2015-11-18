8d141a0f58b46c6378496f4bf12f9f1f74525ea0
Brody Kellish <bd.kellish@gmail.com>
2015-11-18 18:36:13 -0500

    Added test for Simple package

--- working/Main.java
+++ working/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import simple.manager.SimpleGameManager;
 
 public class Main extends Application {
 	
@@ -25,25 +26,27 @@ public class Main extends Application {
 	@Override
 	public void start(Stage arg0) throws Exception {
 		
-//		new AuthoringEnvironment();
+////		new AuthoringEnvironment();
+//		
+//		myGameManager = new GameManager();
+//		
+//		//FIX ALL THIS SHIT LATER
+//		//TODO: REAL IMPLEMENTATION
+//		
+//		Scene scene = this.myGameManager.init(arg0, WIDTH,HEIGHT);
+//		arg0.setTitle(TITLE);
+//		arg0.setScene(scene);
+//		arg0.show();
+//		
+//		KeyFrame frame = new KeyFrame(Duration.seconds(FRAME_LENGTH),
+//                e -> this.myGameManager.update());
+//		Timeline animation = new Timeline();
+//		animation.setCycleCount(Timeline.INDEFINITE);
+//		animation.getKeyFrames().add(frame);
+//		this.myGameManager.setAnimation(animation);
+//		animation.play();
 		
-		myGameManager = new GameManager();
-		
-		//FIX ALL THIS SHIT LATER
-		//TODO: REAL IMPLEMENTATION
-		
-		Scene scene = this.myGameManager.init(arg0, WIDTH,HEIGHT);
-		arg0.setTitle(TITLE);
-		arg0.setScene(scene);
-		arg0.show();
-		
-		KeyFrame frame = new KeyFrame(Duration.seconds(FRAME_LENGTH),
-                e -> this.myGameManager.update());
-		Timeline animation = new Timeline();
-		animation.setCycleCount(Timeline.INDEFINITE);
-		animation.getKeyFrames().add(frame);
-		this.myGameManager.setAnimation(animation);
-		animation.play();
+		SimpleGameManager myGameManager = new SimpleGameManager();
 	}
 
 	public static void main(String[] args) {
