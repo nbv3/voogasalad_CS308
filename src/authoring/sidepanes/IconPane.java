1f52385284da37827c2373bc7cffee650b99c51f
Michael Daou <daoumichael@gmail.com>
2015-11-26 12:40:25 -0500

    Making another method to populate the icon tile pane from an XML directory.

--- src/authoring/sidepanes/IconPane.java
+++ src/authoring/sidepanes/IconPane.java
@@ -1,19 +1,62 @@
 package authoring.sidepanes;
 
+import java.io.File;
+
+import com.syntacticsugar.vooga.util.ResourceManager;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
-public class IconPane extends VBox {
+public class IconPane extends TilePane {
 
 	private int myWidth;
 	private int myHeight;
-	
-	
-	public IconPane(int paneWidth, int paneHeight){
-		this.setHeight(paneHeight);
-		this.setWidth(paneWidth);
+	private ObjectProperty<ImageView> mySelectedIcon = new SimpleObjectProperty<ImageView>();
+	private final int DEFAULT_ICON_DIMENSION = 50;
+
+	public IconPane(int paneWidth, int paneHeight) {
+		super();
+		this.setPrefSize(paneWidth, paneHeight);
+		myWidth = paneWidth;
+		myHeight = paneHeight;
+	}
+
+	protected void showImageOptions(String[] iconPath) {
+		this.getChildren().clear();
+		for (int i = 0; i < iconPath.length; i++) {
+			Image iconImage = new Image(getClass().getClassLoader().getResourceAsStream(iconPath[i]));
+			ImageView icon = new ImageView(iconImage);
+			setIconOnClickVisual(icon);
+			icon.setFitWidth(DEFAULT_ICON_DIMENSION);
+			icon.setFitHeight(DEFAULT_ICON_DIMENSION);
+			this.getChildren().add(icon);
+		}
 	}
 	
-	public void populateIcons(){
+	protected void showImageOptions(File XMLDirectory){
 		
 	}
+	
+	private void setIconOnClickVisual(ImageView icon){
+		icon.setOnMouseClicked(e -> {
+			mySelectedIcon.setValue(icon);
+		});
+		
+		mySelectedIcon.addListener((o,s1,s2) -> {
+			if (s1 == null) {
+				s2.setEffect(new Glow(0.7));
+				return;
+			}
+			
+			s1.setEffect(null);
+			s2.setEffect(new Glow(0.7));
+			});
+		
+	}
+
 }
