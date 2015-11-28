c5b28db9a9237273b54023fb3d27a8d8c05591b0
Henry Yuen <henry.yuen95@gmail.com>
2015-11-28 01:32:11 -0500

    Parameter Factory

---
+++ src/com/syntacticsugar/vooga/util/gui/factory/ParameterFactory.java
@@ -0,0 +1,16 @@
+package com.syntacticsugar.vooga.util.gui.factory;
+
+import com.syntacticsugar.vooga.gameplayer.attribute.IAttribute;
+
+import javafx.scene.Node;
+
+public class ParameterFactory {
+	public ParameterFactory(){
+			
+	}
+	
+	public Node createNode(IAttribute attribute){
+		return null;
+	}
+	
+}
