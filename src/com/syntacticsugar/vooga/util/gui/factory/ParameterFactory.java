ce13662436a554c9d0c3e1404735a26bffd817a5
Henry Yuen <henry.yuen95@gmail.com>
2015-11-28 01:54:46 -0500

    Class[] returns parameters types

--- src/com/syntacticsugar/vooga/util/gui/factory/ParameterFactory.java
+++ src/com/syntacticsugar/vooga/util/gui/factory/ParameterFactory.java
@@ -1,6 +1,10 @@
 package com.syntacticsugar.vooga.util.gui.factory;
 
+import java.lang.reflect.Constructor;
+
+import com.syntacticsugar.vooga.gameplayer.attribute.HealthAttribute;
 import com.syntacticsugar.vooga.gameplayer.attribute.IAttribute;
+import com.syntacticsugar.vooga.gameplayer.attribute.WeaponAttribute;
 
 import javafx.scene.Node;
 
@@ -10,7 +14,15 @@ public class ParameterFactory {
 	}
 	
 	public Node createNode(IAttribute attribute){
+		Constructor<?>[] constructors = attribute.getClass().getDeclaredConstructors();
+		if(constructors.length>1) throw new IllegalArgumentException("This attribute has more than 1 constructor. Please check.");
+		Class[] paramTypes = constructors[0].getParameterTypes();
 		return null;
 	}
 	
+//	public static void main(String[] args){
+//		ParameterFactory paramFactory = new ParameterFactory();
+//		//paramFactory.createNode(new HealthAttribute(100));
+//		paramFactory.createNode(new WeaponAttribute(null, 0, null));
+//	}
 }
