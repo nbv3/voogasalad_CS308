2c86c2a25c5005f55763017b319b13bb4a6496ab
Henry Yuen <henry.yuen95@gmail.com>
2015-11-28 02:30:34 -0500

    Needs to call the method appropriately

--- src/com/syntacticsugar/vooga/util/gui/factory/ParameterFactory.java
+++ src/com/syntacticsugar/vooga/util/gui/factory/ParameterFactory.java
@@ -7,19 +7,35 @@ import com.syntacticsugar.vooga.gameplayer.attribute.IAttribute;
 import com.syntacticsugar.vooga.gameplayer.attribute.WeaponAttribute;
 
 import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
 
 public class ParameterFactory {
 	public ParameterFactory(){
-			
+		
 	}
 	
-	public Node createNode(IAttribute attribute){
+	public Node loadNode(IAttribute attribute){
+	
 		Constructor<?>[] constructors = attribute.getClass().getDeclaredConstructors();
 		if(constructors.length>1) throw new IllegalArgumentException("This attribute has more than 1 constructor. Please check.");
 		Class[] paramTypes = constructors[0].getParameterTypes();
+		for(Class<?> classType:paramTypes){
+			
+		}
 		return null;
 	}
 	
+	public Node createNode(double val){
+		return null;
+	}
+	
+	public Node createNode(String string){
+		return null;
+	}
+	
+	public Node createNode(KeyCode keycode){
+		return null;
+	}	
 //	public static void main(String[] args){
 //		ParameterFactory paramFactory = new ParameterFactory();
 //		//paramFactory.createNode(new HealthAttribute(100));
