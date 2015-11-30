4adf923ac93100a064f0f1b3f6c922a76d6402e1
Elsie Ling <elsieling95@gmail.com>
2015-11-30 00:07:08 -0500

    created object library manager and tabs

--- src/authoring/library/ObjectLibraryManager.java
+++ src/authoring/library/ObjectLibraryManager.java
@@ -1,6 +1,10 @@
 package authoring.library;
 
+import java.io.File;
+
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
 
 public class ObjectLibraryManager {
 
@@ -8,13 +12,36 @@ public class ObjectLibraryManager {
 	private ObjectLibrary myEnemyLibrary;
 	private ObjectLibrary myTowerLibrary;
 	private ObjectLibrary myPlayerLibrary;
-	
-	public ObjectLibraryManager() {
-		initializeLibraryTabs();
+	private ObjectLibrary myItemLibrary;
+
+	public ObjectLibraryManager(File GameDirectory) {
+		initializeLibraryTabs(GameDirectory);
+		myLibraryTabs = new TabPane();
+		populateTabPane();
+		myLibraryTabs.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 	}
-	
-	private void initializeLibraryTabs() {
-		
+
+	private void initializeLibraryTabs(File GameDirectory) {
+		myEnemyLibrary = new EnemyLibrary(GameDirectory);
+		myTowerLibrary = new TowerLibrary(GameDirectory);
+		myPlayerLibrary = new PlayerLibrary(GameDirectory);
+		myItemLibrary = new ItemLibrary(GameDirectory);
 	}
-	
+
+	private void populateTabPane() {
+		Tab myEnemyTab = new Tab("Enemies");
+		myEnemyTab.setContent(myEnemyLibrary.getContent());
+		Tab myTowerTab = new Tab("Towers");
+		myTowerTab.setContent(myTowerLibrary.getContent());
+		Tab myPlayerTab = new Tab("Players");
+		myPlayerTab.setContent(myPlayerLibrary.getContent());
+		Tab myItemTab = new Tab("Items");
+		myItemTab.setContent(myItemLibrary.getContent());
+		myLibraryTabs.getTabs().addAll(myEnemyTab, myTowerTab, myPlayerTab, myItemTab);
+
+	}
+
+	public TabPane getTabPane() {
+		return myLibraryTabs;
+	}
 }
