557b60e016553bd82e2574c6ffb203353e782240
Henry Yuen <henry.yuen95@gmail.com>
2015-12-03 15:59:59 -0500

    Implemented FadeTransitions for SpawnQueue removal

--- src/authoring/fluidmotion/QueueTransitionsWizard.java
+++ src/authoring/fluidmotion/QueueTransitionsWizard.java
@@ -1,5 +1,11 @@
 package authoring.fluidmotion;
 
 public class QueueTransitionsWizard {
-
+	public static void applyHorizontalTransition(){
+		
+	}
+	
+	public static void applyVerticalTransition(){
+		
+	}
 }
