af5d4da2e14128e09a9b9583622696bd72e1c860
Jiawei Zhang <jiaweizhang95@gmail.com>
2015-12-03 02:00:26 -0500

    Removed unused XML utilities and added XML format

--- src/com/syntacticsugar/vooga/util/xml/LoadingException.java
+++
@@ -1,9 +0,0 @@
-package com.syntacticsugar.vooga.util.xml;
-
-public class LoadingException extends Exception {
-	public LoadingException () {
-		/**
-		 * TODO implement LoadingException
-		 */
-	}
-}
