8724ed08a395d7b6f91fa150aedd8c2a329de944
Jiawei Zhang <jiaweizhang95@gmail.com>
2015-12-03 16:28:12 -0500

    Working post request

--- src/com/syntacticsugar/vooga/util/webconnect/WebConnector.java
+++ src/com/syntacticsugar/vooga/util/webconnect/WebConnector.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,11 +17,55 @@ import org.json.JSONObject;
 
 public class WebConnector {
 	public static void main(String args[]) {
-		System.out.println(getXML());
+		//System.out.println(getXML());
+		
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("xml","<something here again>");
+		map.put("author", "Michael");
+		map.put("gamename", "marioch");
+		map.put("description", "some desc");
+				
+
+		
+		JSONObject request = new JSONObject(map);
+		System.out.println(request.toString());
+		postXML(request);
 	}
 	
 	
-	public static void postXML() {
+	public static void postXML(JSONObject json) {
+		String query = json.toString();
+		String url = "http://easywebapi.com/api/newxml";
+		String charset = "UTF-8";  // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
+		URLConnection connection;
+		try {
+			connection = new URL(url).openConnection();
+			connection.setDoOutput(true); // Triggers POST.
+			connection.setRequestProperty("Accept-Charset", charset);
+			connection.setRequestProperty("Content-Type", "application/json;charset=" + charset);
+
+			try (OutputStream output = connection.getOutputStream()) {
+			    output.write(query.getBytes(charset));
+			}
+
+			InputStream response = connection.getInputStream();
+			
+			BufferedReader streamReader = new BufferedReader(new InputStreamReader(response, charset)); 
+			StringBuilder responseStrBuilder = new StringBuilder();
+
+			String inputStr;
+			while ((inputStr = streamReader.readLine()) != null)
+			    responseStrBuilder.append(inputStr);
+			
+			System.out.println(responseStrBuilder.toString());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
