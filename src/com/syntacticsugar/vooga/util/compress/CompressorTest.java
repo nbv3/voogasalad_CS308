0856f24ff268baef199378a0024f862406333897
Jiawei Zhang <jiaweizhang95@gmail.com>
2015-12-03 20:18:58 -0500

    Working String-byte[] compression and uncompression

--- src/com/syntacticsugar/vooga/util/compress/CompressorTest.java
+++ src/com/syntacticsugar/vooga/util/compress/CompressorTest.java
@@ -1,14 +1,29 @@
 package com.syntacticsugar.vooga.util.compress;
 
 import java.io.File;
-
+import java.nio.file.Files;
 
 public class CompressorTest {
 	public static void main(String args[]) {
-		
+
 		File selectedFile = new File("src/com/syntacticsugar/vooga/util/compress/Untitled.xml");
-		System.out.println(selectedFile.getName());
-		System.out.println(selectedFile.length());
-		Compressor.compressTest(selectedFile);
+		
+		String inputString = readFile(selectedFile);
+		System.out.printf("%30s: %8d\n", "Input string size", inputString.length());
+		byte[] compressed = Compressor.compress(inputString);
+		System.out.printf("%30s: %8d\n", "Compressed string size", compressed.length);
+		String uncompressed = Compressor.uncompress(compressed);
+		System.out.printf("%30s: %8d\n", "Uncompressed string size", uncompressed.length());
+		System.out.printf("%30s: %8b\n", "Equal", inputString.equals(uncompressed));
+	}
+
+	private static String readFile(File f) {
+		try {
+			byte[] inputBytes = Files.readAllBytes(f.toPath());
+			return new String(inputBytes);
+		} catch (Exception e) {
+
+		}
+		return "";
 	}
 }
