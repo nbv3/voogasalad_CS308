66cece28e45f3e8c3fdb592acf83d252d22e051f
Michael Daou <daoumichael@gmail.com>
2015-12-03 21:17:16 -0500

    Need code from master, switching over now.

---
+++ src/com/syntacticsugar/vooga/social/XMLViewer.java
@@ -0,0 +1,19 @@
+package com.syntacticsugar.vooga.social;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import com.syntacticsugar.vooga.util.ResourceManager;
+import com.syntacticsugar.vooga.util.gui.factory.GUIFactory;
+
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class XMLViewer {
+
+
+}
