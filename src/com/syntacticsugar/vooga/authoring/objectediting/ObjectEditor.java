4308286d05b931c3b4419ef2e753f2febdd3c4df
Jiadong Yin <jy114@duke.edu>
2015-12-04 02:49:26 -0500

    Add the store and save function for the ObjectEditor

--- src/com/syntacticsugar/vooga/authoring/objectediting/ObjectEditor.java
+++ src/com/syntacticsugar/vooga/authoring/objectediting/ObjectEditor.java
@@ -1,9 +1,12 @@
 package com.syntacticsugar.vooga.authoring.objectediting;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import com.syntacticsugar.vooga.authoring.icon.Icon;
 import com.syntacticsugar.vooga.authoring.library.IRefresher;
+import com.syntacticsugar.vooga.gameplayer.attribute.HealthAttribute;
 import com.syntacticsugar.vooga.gameplayer.objects.GameObjectType;
 import com.syntacticsugar.vooga.util.ResourceManager;
 import com.syntacticsugar.vooga.util.gui.factory.AlertBoxFactory;
@@ -11,6 +14,7 @@ import com.syntacticsugar.vooga.util.gui.factory.GUIFactory;
 import com.syntacticsugar.vooga.xml.XMLHandler;
 import com.syntacticsugar.vooga.xml.data.ObjectData;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
@@ -19,6 +23,7 @@ import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
@@ -35,23 +40,51 @@ import javafx.stage.FileChooser.ExtensionFilter;
 public class ObjectEditor {
 
 	private GridPane myView;
+	private ObjectData currentData;
 	private AttributeViewer myAttributeViewer;
 	private CollisionViewer myCollisionViewer;
 	private Icon myIcon;
 	private Button myCreateButton;
-	private ObjectData currentData;
+	private Button mySaveButton;
 	private IRefresher myRefresher;
 
 	public ObjectEditor(IRefresher refresher){
+		myView = new GridPane();
+		myView.setAlignment(Pos.CENTER);
 		currentData = new ObjectData();
-		currentData.setType(GameObjectType.ENEMY);
 		myAttributeViewer = new AttributeViewer();
 		myCollisionViewer = new CollisionViewer();
 		myRefresher = refresher;
-		myView = buildView();			
+		buildView();
+	}
+
+	private void buildView() {
+		GridPane myMainEditorView = buildEditorView();
+		ComboBox<String> myTypeChooser = buildTypeChooser();
+		AnchorPane myTopControlPane = GUIFactory.buildAnchorPane(myTypeChooser, GUIFactory.buildButton("Create", e -> createEmptyEditor(myTypeChooser), null, null));
+		mySaveButton = GUIFactory.buildButton("Save Object", e -> storeEditedObject(), null, null);
+		myCreateButton = GUIFactory.buildButton("Create Object", e -> saveObject(), null, null);
+		AnchorPane myBottomControlPane = GUIFactory.buildAnchorPane(mySaveButton, myCreateButton);
+		myView.add(myTopControlPane, 0, 0, 1, 1);
+		myView.add(myMainEditorView, 0, 1, 1, 1);
+		myView.add(myBottomControlPane, 0, 2, 1, 1);
+		GridPane.setHalignment(myTopControlPane, HPos.CENTER);
+		GridPane.setHalignment(myMainEditorView, HPos.CENTER);
 	}
 	
-	private GridPane buildView() {
+	private void createEmptyEditor(ComboBox<String> cBox) {
+		cBox.setValue(null);
+		ObjectData emptyData = new ObjectData();
+		emptyData.setImagePath("gray.png");
+		myIcon.setImage(new Image(ResourceManager.getResource(this, emptyData.getImagePath())));
+		emptyData.setObjectName(null);
+		emptyData.setType(null);
+		emptyData.setAttributes(FXCollections.observableArrayList());
+		emptyData.setCollisionMap(FXCollections.observableHashMap());
+		displayData(emptyData);
+	}
+
+	private GridPane buildEditorView() {
 		AnchorPane attributeAnchor = buildButtons(
 				e -> buildNewAttribute(),
 				e -> myAttributeViewer.removeSelectedItem(),
@@ -62,7 +95,7 @@ public class ObjectEditor {
 				e -> myCollisionViewer.removeSelectedItem(),
 				"Collisions Defined");
 		
-		GridPane grid = createMainGrid();
+		GridPane grid = createMainEditorGrid();
 				
 		VBox att = createEditorVBox(attributeAnchor, myAttributeViewer.getView());
 		
@@ -70,35 +103,62 @@ public class ObjectEditor {
 		
 		GridPane iconGrid = createIconGrid();
 		
-		myCreateButton = GUIFactory.buildButton("Save Object", e -> saveObject(), null, null);
-		myCreateButton.setAlignment(Pos.CENTER_RIGHT);
-		myCreateButton.setDisable(true);
+//		mySaveButton = GUIFactory.buildButton("Save Object", e -> storeEditedObject(), null, null);
+//		mySaveButton.setAlignment(Pos.CENTER);
+//		mySaveButton.setDisable(true);
+//		
+//		myCreateButton = GUIFactory.buildButton("Create Object", e -> saveObject(), null, null);
+//		myCreateButton.setAlignment(Pos.CENTER);
+//		myCreateButton.setDisable(true);
 		
 		grid.add(iconGrid, 0, 0, 1, 1);
 		grid.add(coll, 0, 1, 2, 1);
 		grid.add(att, 1, 0, 1, 1);
-		grid.add(myCreateButton, 1, 2, 1, 1);
-		GridPane.setHalignment(myCreateButton, HPos.RIGHT);
-		GridPane.setValignment(myCreateButton, VPos.CENTER);
 		return grid;
 	}
 
+	private ComboBox<String> buildTypeChooser() {
+		ComboBox<String> typeChooser = new ComboBox<String>();
+		typeChooser.setPromptText(ResourceManager.getString("ChooseObjectType"));
+		typeChooser.setPrefWidth(150);
+		for (GameObjectType g: GameObjectType.values()) {
+			typeChooser.getItems().add(g.toString());
+		}
+		typeChooser.valueProperty().addListener((o, s1, s2) -> {
+			if (s2 == null) {
+				return;
+			}
+			currentData.setType(GameObjectType.valueOf(s2.toUpperCase()));
+		});
+		return typeChooser;
+	}
+
 	public void displayData(ObjectData data) {
-		if (data != null) {
+		if (currentData != null) {
 			if (myCreateButton.isDisabled()) {
 				myCreateButton.setDisable(false);
 			}
-			this.currentData = data;
-			myAttributeViewer.displayData(data.getAttributes());
-			myCollisionViewer.displayData(data.getCollisionMap());
-			myIcon.setImage(new Image(ResourceManager.getResource(this, data.getImagePath())));
+			currentData = data;
+			myAttributeViewer.displayData(currentData.getAttributes());
+			myCollisionViewer.displayData(currentData.getCollisionMap());
+			myIcon.setImage(new Image(ResourceManager.getResource(this, currentData.getImagePath())));
 		}
 	}
 	
-	private void saveObject() {
+	private void storeEditedObject() {
 		currentData.setAttributes(myAttributeViewer.getData());
 		currentData.setCollisionMap(myCollisionViewer.getData());
 		currentData.setType(currentData.getType());
+	}
+	
+	private void saveObject() {
+		GameObjectType tempObjType = currentData.getType();
+		String tempImgPath = new String(currentData.getImagePath());
+		currentData = new ObjectData();
+		currentData.setType(tempObjType);
+		currentData.setImagePath(tempImgPath);
+		currentData.setAttributes(Collections.unmodifiableCollection(myAttributeViewer.getData()));
+		currentData.setCollisionMap(Collections.unmodifiableMap(myCollisionViewer.getData()));
 		
 		TextInputDialog td = new TextInputDialog("Name your creation");
 		td.showAndWait();
@@ -130,8 +190,17 @@ public class ObjectEditor {
 	private void selectImage() {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.getExtensionFilters().add(new ExtensionFilter("Image Files", "*.jpeg", "*.gif", "*.png"));
-		fileChooser.setTitle("Save Resource File");
-		fileChooser.setInitialDirectory(new File(ResourceManager.getString("images")));
+		fileChooser.setTitle("Save Resource File");	
+		if (currentData.getType() == null) {
+			AlertBoxFactory.createObject(ResourceManager.getString("select_object_type_error"));
+			return;
+		}
+		fileChooser.setInitialDirectory(
+				new File(
+						ResourceManager.getString(
+								String.format("%s_%s", 
+										currentData.getType().toString().toLowerCase(), 
+										"images"))));
 		File selectedFile = fileChooser.showOpenDialog(new Stage());
 		if (selectedFile != null) {
 			currentData.setImagePath(selectedFile.getName());
@@ -140,6 +209,10 @@ public class ObjectEditor {
 	}
 	
 	private void buildNewAttribute() {
+		if (currentData.getType() == null) {
+			AlertBoxFactory.createObject(ResourceManager.getString("select_object_type_error"));
+			return;
+		}
 		new AttributeMakerWizard(currentData.getType(), myAttributeViewer.getData());
 	}
 	
@@ -174,7 +247,7 @@ public class ObjectEditor {
 		return att;
 	}
 
-	private GridPane createMainGrid() {
+	private GridPane createMainEditorGrid() {
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.getColumnConstraints().addAll(
