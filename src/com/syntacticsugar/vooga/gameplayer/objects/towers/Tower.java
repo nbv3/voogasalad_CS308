e62bfc670db089035aadd4d18094f879c367698f
jack-gillette <jackackckk@gmail.com>
2015-12-05 21:25:24 -0500

    add timed despawn attribute

--- src/com/syntacticsugar/vooga/gameplayer/objects/towers/Tower.java
+++ src/com/syntacticsugar/vooga/gameplayer/objects/towers/Tower.java
@@ -2,12 +2,13 @@ package com.syntacticsugar.vooga.gameplayer.objects.towers;
 
 import com.syntacticsugar.vooga.gameplayer.objects.GameObject;
 import com.syntacticsugar.vooga.xml.data.ObjectData;
+import com.syntacticsugar.vooga.xml.data.TowerData;
 
 public class Tower extends GameObject implements ITower {
 	
 	private int myCost;
 	
-	public Tower(ObjectData data) {
+	public Tower(TowerData data) {
 		super(data);
 	}
 	
